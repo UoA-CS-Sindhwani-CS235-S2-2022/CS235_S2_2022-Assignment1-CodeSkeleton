0c78417509976279f8802cecf736d5dbd90ece83
Jessi Liao <wlia371@aucklanduni.ac.nz>
2022-08-15 22:40:33 +0800

    cr已过,但存在hidden

--- domainmodel/artist.py
+++ domainmodel/artist.py
@@ -1,9 +1,10 @@
 class Artist:
     def __init__(self, artist_id: int, full_name: str):
-        # TODO
         self.__artist_id: int = artist_id
-        self.__full_name = None
-        pass
+        if type(full_name) == int or full_name.strip(" ") == '':
+            self.__full_name = "None"
+        else:
+            self.__full_name = full_name.strip(' ')
 
     @property
     def artist_id(self) -> int:
@@ -15,8 +16,8 @@ class Artist:
 
     @full_name.setter
     def full_name(self, new_full_name):
-        # TODO
-        pass
+        if type(new_full_name) != int and new_full_name.strip(" ") != '':
+            self.__full_name = new_full_name.strip(' ')
 
     def __repr__(self):
         # we use access via the property here
@@ -28,9 +29,10 @@ class Artist:
         return self.artist_id == other.artist_id
 
     def __lt__(self, other):
-        # TODO
-        pass
+        if self.__artist_id<other.__artist_id:
+            return self.__full_name<other.__full_name
+        else:
+            return self.__full_name>other.__full_name
 
     def __hash__(self):
-        # TODO
-        pass
+        return hash((self.__artist_id(),self.__full_name()))
