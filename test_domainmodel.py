df241dfb49574670d958bf282b6bdc61cb17d188
Jessi Liao <wlia371@aucklanduni.ac.nz>
2022-08-16 00:55:23 +0800

    add genre test_domainmodel

--- test_domainmodel.py
+++ test_domainmodel.py
@@ -1,6 +1,7 @@
 import pytest
 
 from domainmodel.artist import Artist
+from domainmodel.genre import Genre
 
 
 class TestArtist:
@@ -18,3 +19,18 @@ class TestArtist:
         assert str(artist5.full_name) == 'None'
         artist6 = Artist(-1, 'Tailor Swift')# Test when the artist id type is invalid
         assert str(artist6) == 'This is invalid artist id'
+
+class TestGener:
+
+    def test_construction(self):
+        genre1 = Genre(1, 'Jazz ')
+        assert str(genre1) == '<Genre Jazz, genre id = 1>'
+        genre2 = Genre(2, ' Electronic ')
+        assert str(genre2) == '<Genre Electronic, genre id = 2>'
+        genre3 = Genre(3, 300) # Test invalid types for a name sets it to None
+        assert str(genre3.name) == 'None'
+        genre4 = Genre(1, 'Jazz')
+        genre4.name = 'New Jazz'
+        assert str(genre4) == '<Genre New Jazz, genre id = 1>'
+        genre5 = Genre(-2, 'Jazz')# Test invalid id raises error
+        assert str(genre5) == 'This is invalid gener id'
